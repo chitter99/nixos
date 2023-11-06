fc1f677504a36c99dcd3cc316f4300fc770a4237
Aaron Schmid <dev@clab.rocks>
2023-11-06 11:46:26 +0100

    setup ssh-agent

--- modules/apps/1password/default.nix
+++ modules/apps/1password/default.nix
@@ -6,10 +6,15 @@
 Host *
   IdentityAgent ~/.1password/agent.sock
       '';
+      ".config/1Password/ssh/agent.toml".text = ''
+[[ssh-keys]]
+vault = "SSH"
+      '';
      };
    };
    programs.bash.interactiveShellInit = ''
 source /home/${username}/.config/op/plugins.sh
+export SSH_AUTH_SOCK=~/.1password/agent.sock
    '';
    programs = {
     _1password.enable = true;
