90bca18adc94f62c795de5cb3c87c8697be9e272
Aaron Schmid <dev@clab.rocks>
2023-11-07 22:14:30 +0100

    user default shell

--- users/arsch/default.nix
+++ users/arsch/default.nix
@@ -2,7 +2,7 @@
 {
 # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.arsch = {
-    shell = pkgs.bash;
+    shell = pkgs.fish;
     isNormalUser = true;
     initialPassword = "123456";
     extraGroups = [ "wheel" "input" "render" "audio" "networkmanager" ];
