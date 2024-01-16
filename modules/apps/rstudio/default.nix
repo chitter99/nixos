bcf993bfca61308b739bfe101278918d0f39ecb4
Aaron Schmid <aaron@clab.rocks>
2024-01-16 10:54:21 +0100

    fixed syntax issue in rstudio app

--- modules/apps/rstudio/default.nix
+++ modules/apps/rstudio/default.nix
@@ -3,6 +3,7 @@ let
   rstudio = pkgs.rstudioWrapper.override{
     packages = with pkgs.rPackages; []; 
   };
+in
 {
   home-manager.users.${username} = { pkgs, ... }: {
     home.packages = with pkgs; [ rstudio ];
