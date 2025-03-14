3a87200af771a9f836c7611b1a76d61decf5b0aa
Aaron Schmid <aaron@clab.rocks>
2025-03-14 14:29:33 +0100

    added gaming stuff

--- modules/apps/gaming/default.nix
+++ modules/apps/gaming/default.nix
@@ -1,6 +1,6 @@
 { pkgs, home-manager, username, ... }: {
   home-manager.users.${username} = { pkgs, ... }: {
-    home.packages = with pkgs; [ mangohud protonup ];
+    home.packages = with pkgs; [ mangohud protonup lutris bottles ];
 
     home.sessionVariables = {
       STEAM_EXTRA_COMPAT_TOOLS_PATHS =
