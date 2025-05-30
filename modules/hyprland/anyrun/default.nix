a5a046aeb2a8a32a5af0d3961f8ad2e542fb5beb
Aaron Schmid <aaron@clab.rocks>
2025-05-30 10:48:46 +0200

    anyrun config

--- modules/hyprland/anyrun/default.nix
+++ modules/hyprland/anyrun/default.nix
@@ -3,11 +3,11 @@
     programs.anyrun = {
       enable = true;
       config = {
-
+        x = { fraction = 0.5; };
+        y = { fraction = 0.3; };
         width = { fraction = 0.5; };
-        hidePluginInfo = true;
-        layer = "overlay";
 
+        layer = "overlay";
         closeOnClick = true;
 
         plugins = [
@@ -20,23 +20,34 @@
           "${pkgs.anyrun}/lib/libwebsearch.so"
         ];
       };
-
-      # Inline comments are supported for language injection into
-      # multi-line strings with Treesitter! (Depends on your editor)
       extraCss = # css
         ''
-          .some_class {
-            background: red;
+          #window {
+            background-color: rgba(0, 0, 0, 0);
+          }
+
+          box#main {
+            border-radius: 10px;
+            background-color: @theme_bg_color;
+          }
+
+          list#main {
+            background-color: rgba(0, 0, 0, 0);
+            border-radius: 10px;
+          }
+
+          list#plugin {
+            background-color: rgba(0, 0, 0, 0);
+          }
+
+          label#match-desc {
+            font-size: 10px;
+          }
+
+          label#plugin {
+            font-size: 14px;
           }
         '';
-
-      extraConfigFiles."some-plugin.ron".text = ''
-        Config(
-          // for any other plugin
-          // this file will be put in ~/.config/anyrun/some-plugin.ron
-          // refer to docs of xdg.configFile for available options
-        )
-      '';
     };
   };
 }
