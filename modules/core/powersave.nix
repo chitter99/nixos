62b40d83fa7a6b88aff1a3458f6906297e39f269
Aaron Schmid <aaron@clab.rocks>
2025-06-16 13:46:50 +0200

    updated and made sure tlp is disabled

--- modules/core/powersave.nix
+++ modules/core/powersave.nix
@@ -3,4 +3,5 @@
   # Need to disable as its enabled in the gnome stack
   # See https://discourse.nixos.org/t/cant-enable-tlp-when-upgrading-to-21-05/13435/2
   services.power-profiles-daemon.enable = false;
+  services.tlp.enable = false;
 }
